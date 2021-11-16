edaf695d0c1fa19fb60d4b6270976ac00796eb18
Charlotte Edwin <edwin.charlotte@etu.univ-orleans.fr>
2021-11-16 16:05:13 +0100

    Ajout et correction errors

--- azure-function/node/GetBusData/index.js
+++ azure-function/node/GetBusData/index.js
@@ -5,33 +5,6 @@ const AZURE_CONN_STRING = process.env.AzureSQLConnectionString;
 const GTFS_RT_FEED = process.env.RealTimeFeedUrl;
 const LOGIC_APP_URL = process.env.LogicAppUrl;
 
-module.exports = async function (context, myTimer) {
-  // Get the routes we want to monitor
-  const routes = await GetMonitoredRoutes();
-
-  // Get the real-time bus location feed
-  const feed = await GetRealTimeFeed();
-
-  // Filter only the routes we want to monitor
-  const buses = feed.entity.filter((e) => routes.includes(parseInt(e.vehicle.trip.route_id)));
-
-  context.log(`Received ${feed.entity.length} buses positions, found ${buses.length} buses in monitored routes`);
-
-  // Push data to Azure SQL and get the activated geofences
-  const activatedGeofences = await ProcessGeoFences(context, buses);
-
-  // Send notifications
-  // (using 'map' instead of 'forEach' to make sure all calls are awaited: https://advancedweb.hu/how-to-use-async-functions-with-array-foreach-in-javascript/)
-  await Promise.all(
-    activatedGeofences.map(async (gf) => {
-      context.log(
-        `Vehicle ${gf.VehicleId}, route ${gf.RouteId}, ${gf.GeoFenceStatus} Geofence ${gf.GeoFence} at ${gf.TimestampUTC} UTC.`
-      );
-      await TriggerLogicApp(context, gf);
-    }),
-  );
-};
-
 async function GetMonitoredRoutes() {
   const pool = await sql.connect(AZURE_CONN_STRING);
   const queryResult = await pool.request().execute('web.GetMonitoredRoutes');
@@ -45,6 +18,27 @@ async function GetRealTimeFeed() {
   const feed = await response.json();
 
   return feed;
+}
+
+async function TriggerLogicApp(context, geofence) {
+  const content = {
+    value1: geofence.VehicleId,
+    value2: geofence.GeoFenceStatus,
+  };
+
+  context.log(`Calling Logic App webhook for ${geofence.VehicleId}`);
+
+  try {
+    const response = await fetch(LOGIC_APP_URL, {
+      method: 'post',
+      body: JSON.stringify(content),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) context.log(`[${geofence.VehicleId}/${geofence.DirectionId}/${geofence.GeoFenceId}] WebHook called successfully`);
+    else context.log(`Error calling Logic App. HTTP Response is: ${response.status}`);
+  } catch (err) {
+    context.log(`Error calling Logic App. Error is: ${err}`);
+  }
 }
 
 async function ProcessGeoFences(context, buses) {
@@ -73,24 +67,29 @@ async function ProcessGeoFences(context, buses) {
   return geoFences;
 }
 
-async function TriggerLogicApp(context, geofence) {
-  const content = {
-    value1: geofence.VehicleId,
-    value2: geofence.GeoFenceStatus,
-  };
+module.exports = async function (context, myTimer) {
+  // Get the routes we want to monitor
+  const routes = await GetMonitoredRoutes();
 
-  context.log(`Calling Logic App webhook for ${geofence.VehicleId}`);
+  // Get the real-time bus location feed
+  const feed = await GetRealTimeFeed();
 
-  try {
-    const response = await fetch(LOGIC_APP_URL, {
-      method: 'post',
-      body: JSON.stringify(content),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok)
-      context.log(`[${geofence.VehicleId}/${geofence.DirectionId}/${geofence.GeoFenceId}] WebHook called successfully`);
-    else context.log(`Error calling Logic App. HTTP Response is: ${response.status}`);
-  } catch (err) {
-    context.log(`Error calling Logic App. Error is: ${err}`);
-  }
-}
+  // Filter only the routes we want to monitor
+  const buses = feed.entity.filter((e) => routes.includes(parseInt(e.vehicle.trip.route_id)));
+
+  context.log(`Received ${feed.entity.length} buses positions, found ${buses.length} buses in monitored routes`);
+
+  // Push data to Azure SQL and get the activated geofences
+  const activatedGeofences = await ProcessGeoFences(context, buses);
+
+  // Send notifications
+  // (using 'map' instead of 'forEach' to make sure all calls are awaited: https://advancedweb.hu/how-to-use-async-functions-with-array-foreach-in-javascript/)
+  await Promise.all(
+    activatedGeofences.map(async (gf) => {
+      context.log(
+        `Vehicle ${gf.VehicleId}, route ${gf.RouteId}, ${gf.GeoFenceStatus} Geofence ${gf.GeoFence} at ${gf.TimestampUTC} UTC.`,
+      );
+      await TriggerLogicApp(context, gf);
+    }),
+  );
+};
